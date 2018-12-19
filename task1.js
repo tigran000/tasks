6ac08faafdaeb61470adc99428a00eb08f579ed9
karabala <tigranharutyunyan59@gmail.com>
2018-12-19 13:20:59 -0800

    first commit

---
+++ task1.js
@@ -0,0 +1,32 @@
+const faker = require('faker');
+
+class Person {
+  constructor(name, age) {
+    this.age = age
+    this.name = name
+    setInterval(() => this.age++, 1000)
+  }
+}
+
+const checkAge = People => {
+  setInterval(() => console.log(People.filter(Person => Person.age <= 15)), 1000)
+}
+
+const addPerson = People => {
+  setInterval(cb, 2000)
+
+  function cb() {
+    const newPerson = new Person(faker.name.findName(),
+      Math.floor((Math.random() * 50) + 1))
+    People.push(newPerson)
+  }
+}
+
+const Armen = new Person('Armen', 15)
+const Ann = new Person('Ann', 0)
+const Areg = new Person('Areg', 5)
+const Arsen = new Person('Arsen', 12)
+const People = [Armen, Ann, Areg, Arsen]
+
+checkAge(People)
+addPerson(People)
